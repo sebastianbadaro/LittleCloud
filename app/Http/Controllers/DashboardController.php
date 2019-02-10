ae963d53a4db9fd50c54429ca0166bf1c5a2c585
Sebastian <sebastianbadaro@gmail.com>
2019-02-10 14:12:45 -0300

    bar chart for payment type on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,6 +17,8 @@ class DashboardController extends Controller
   public function show()
   {
 
+    //TODO: move this logic to each model
+
     $categorias = DB::table('categories')
             ->join('products', 'categories.id', '=', 'products.category_id')
             ->select(DB::raw('categories.name, sum(products.stock) as count'))
@@ -32,12 +34,16 @@ class DashboardController extends Controller
             ->select(DB::raw('genders.name, count(*) as count'))
             ->groupBy('genders.name')
             ->get();
-
+    $paymentTypes = DB::table('sales')
+            ->join('payment_types', 'payment_types.id', '=', 'sales.paymentType_id')
+            ->select(DB::raw('payment_types.name, count(*) as count'))
+            ->groupBy('payment_types.name')
+            ->get();
 
     // dd($categorias);
     $totalValueOfStock= Product::valueOfStock();
     $amountofItemsInStock = Product::amountofItemsInStock();
     $totalAmountOfClients = Client::totalAmountOfClients();
-    return view('dashboards.dashboard',compact('totalValueOfStock','amountofItemsInStock','totalAmountOfClients','categorias','brands','genders'));
+    return view('dashboards.dashboard',compact('totalValueOfStock','amountofItemsInStock','totalAmountOfClients','categorias','brands','genders','paymentTypes'));
   }
 }
--- resources/views/dashboards/dashboard.blade.php
+++ resources/views/dashboards/dashboard.blade.php
@@ -132,6 +132,26 @@
   </div>
   <!-- /.box -->
 
+  <div class="col-md-12 col-xs-12">
+    <!-- DONUT CHART -->
+    <div class="box box-danger">
+      <div class="box-header with-border">
+        <h3 class="box-title">Ventas por tipo de pago</h3>
+
+        <div class="box-tools pull-right">
+          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+        </div>
+      </div>
+      <div class="box-body" style="height: 300px">
+        <canvas id="paymentTypeChart" width="50" height="50"></canvas>
+      </div>
+      <!-- /.box-body -->
+    </div>
+  </div>
+  <!-- /.box -->
+
   <script>
   var ctx = document.getElementById("brandChart").getContext('2d');
   var myChart = new Chart(ctx, {
@@ -227,6 +247,50 @@
       options: {
         circumference: Math.PI,
         rotation: Math.PI
+      }
+  });
+  </script>
+
+  <script>
+  var ctx = document.getElementById("paymentTypeChart").getContext('2d');
+  ctx.height = 100;
+  var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: {!!json_encode($paymentTypes->pluck("name"))!!},
+          datasets: [{
+              label: '# de ventas',
+              data: {!!json_encode($paymentTypes->pluck("count"))!!},
+              backgroundColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+
+
+              ],
+              borderColor: [
+                  "black",
+                  "black",
+                  "black",
+                  "black",
+                  "black",
+
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
       }
   });
   </script>
